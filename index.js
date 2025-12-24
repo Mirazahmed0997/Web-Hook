c8bae89ac1e9e06c269619f384df04fdb5048392
Miraz Ahmed <ahmedmiraz87@gmail.com>
2025-12-24 18:19:10 +0600

    singleOrderAPI

--- index.js
+++ index.js
@@ -6,19 +6,25 @@ import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json()); // parse JSON bodies
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true,              
+  })
+);
+
+app.use(express.json());
 
 
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
-  .connect(MONGO_URI) // no extra options needed
+  .connect(MONGO_URI) 
   .then(() => console.log(" MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));
 
 
-const orderSchema = new mongoose.Schema({}, { strict: false }); // store anything
+const orderSchema = new mongoose.Schema({}, { strict: false }); 
 const Order = mongoose.model("Order", orderSchema);
 
 
@@ -27,10 +33,10 @@ app.post("/apps/cod-order", async (req, res) => {
     const body = req.body;
     console.log("New COD Order Received:", body);
 
-    const order = new Order(body);  // store the whole payload
+    const order = new Order(body);  
     await order.save();
 
-    res.status(200).json({ success: true, received: body, message: "Order saved to DB ✅" });
+    res.status(200).json({ success: true, received: body, message: "Order saved to DB " });
   } catch (err) {
     console.error("Error saving order:", err);
     res.status(500).json({ success: false, error: err.message });
@@ -46,6 +52,29 @@ app.get("/apps/cod-order", async (req, res) => {
     res.status(500).json({ success: false, error: err.message });
   }
 });
+
+// GET /apps/cod-order/:id
+app.get("/apps/cod-order/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    console.log(id)
+
+    const order = await Order.findById(id);
+
+    if (!order) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Order not found" });
+    }
+
+    res.status(200).json({ success: true, order });
+  } catch (err) {
+    console.error("Error fetching single order:", err);
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
 
 
 app.get("/", (req, res) => res.send("Server running"));
